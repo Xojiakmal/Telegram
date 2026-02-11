f4e46eca1ea40a23baf0b3771b5854bda039f6c8
sallomuzb87-start <sallomuzb87@gmail.com>
2026-02-11 20:52:16 +0500

    Update README.php

--- README.php
+++ README.php
@@ -1,9 +1,19 @@
-<?php
-echo "salom";
-
-for ($i=0; $i <=10 ; $i++) { 
-            echo "muhammaddamin aka nima gap";
-        }
-for ($i=0; $i <=10 ; $i++) { 
-            echo "hech gap yoq";
-        }
+ <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            <label for="text">qidirish</label>
+            <input type="text" name="name" id="qidirish">
+            <input type="submit" value="jonatish">
+        </form>
+        <div class="list">
+            <!-- foreach boladi  -->
+            <li><a href="?">username</a></li>
+        </div>
+    </body>
+    </html>
